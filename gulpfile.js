9b56affb5e86feae633cd43fa4024b91f8246c93
Marcos Pinilla <lda_pinigol@hotmail.com>
2018-08-08 23:39:29 -0400

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const ghPages = require('gulp-gh-pages');
 const HubRegistry = require('gulp-hub');
 const browserSync = require('browser-sync');
 
@@ -18,11 +19,7 @@ gulp.task('serve', gulp.series('inject', 'watch', 'browsersync'));
 gulp.task('serve:dist', gulp.series('default', 'browsersync:dist'));
 gulp.task('default', gulp.series('clean', 'build'));
 gulp.task('watch', watch);
-
-gulp.task('deploy', ['default'], () => {
-  return gulp.src('dist/**/*')
-    .pipe($.ghPages());
-});
+gulp.task('deploy', () => src('./dist/**/*').pipe(ghPages()));
 
 function reloadBrowserSync(cb) {
   browserSync.reload();
